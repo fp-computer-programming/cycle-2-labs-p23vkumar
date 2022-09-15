313751d665bb74dd5ec2f99f4cf7836f6cc4abf6
Vishnu Kumar <p23vkumar@fairfieldprep.org>
2022-09-15 10:43:33 -0400

    Lab 2-3
    Lab 2-3

---
+++ lab 2-3.py
@@ -0,0 +1,29 @@
+#1. Create a Python file named lab_2-3.py
+
+#2. Set a equal to 2. 
+# Using the type() function, print the type of variable a
+
+a = 2
+print(type(a))
+
+#3. Change the type of a to a float using a = float(a). What is the result of printing type(a) now?
+
+a = float(a)
+print(a)
+
+# The result of the float of the type function is 2.0
+
+#4. Convert a to a string. Check its type with a print statement. What has changed?
+
+a = str(a)
+print(a)
+
+# Nothing has changed, the value is still 2.0 similar to its float
+
+#5. Convert a to a Boolean. Print the result of a == 2. What is the result? Why? (Answer as a comment)
+
+a = bool(a)
+print(a)
+print( a == 2)
+
+#The result is it is a false value because 
