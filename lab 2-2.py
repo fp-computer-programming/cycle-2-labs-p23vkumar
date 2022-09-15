c4306ae9a8eff9e2b39b75900dc57217840cad36
Vishnu Kumar <p23vkumar@fairfieldprep.org>
2022-09-15 10:49:58 -0400

    Lab 2-2
    Lab 2-2

---
+++ lab 2-2.py
@@ -0,0 +1,28 @@
+#1. Create a Python file named lab_2-2.py
+
+#2. Set a equal to 3 and b equal to a
+
+a = 3
+b = a
+
+#3. Print the value of b and the value of a.
+
+print(b)
+print(a)
+
+#4. What is the value of each variable and why? (Answer as a comment)
+
+#The value of a is 3 and the value b is 3 because since b = a, using the transitive propoerty of congruence, since a = 3, b = 3
+
+#5. Set a equal to 7.
+
+a = 7
+
+#6. Print the value of a and the value of b
+
+print(a)
+print(b)
+
+#7. What is the value of each variable and why? (Answer as a comment)
+
+# The value of a is 7 and the value of b is 3 because a is a new input and therefore produces a different value but the value of b still results in the original output of 3  of the original condition
