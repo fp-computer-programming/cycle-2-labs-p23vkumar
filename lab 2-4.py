0923ee87e685a3a72c38ebe47d487fc4050150e8
Vishnu Kumar <p23vkumar@fairfieldprep.org>
2022-09-16 12:11:10 -0400

    Lab 2-4
    Lab 2-4

---
+++ lab 2-4.py
@@ -0,0 +1,29 @@
+#1. Create a python file named lab_2-4.py
+#2. Translate the following expression into a line of code that can be
+#solved in Python:
+#3. Set the variables equal to the following values to see if you 
+# formatted the equation correctly (The result should be 31)
+#4. Revise your code as needed. 
+
+#Define the variables before setting up the command of pemdas
+a = 1
+b = 2
+c = 3
+x = 2
+y = 5
+
+#Seperate Functions 
+y1 = (3 + 4*x)/5
+print(y1)
+y2 = (10*(y-5)*(a+b+c)**2)/2
+print(y2)
+x1 = 4/x**2
+print(x1)
+x2 = (9+x)/y
+print(x2)
+x3 = 9 * (x1 + x2)
+print(x3)
+
+#Add them all up
+z4 = y1 - y2 + x3
+print(z4)
