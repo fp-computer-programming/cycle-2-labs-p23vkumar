f1045d02259640efce796b142c7ab2c84cb0029b
Vishnu Kumar <p23vkumar@fairfieldprep.org>
2022-09-19 12:00:35 -0400

    Lab 2-5
    Lab 2-5

---
+++ lab 2-5.py
@@ -0,0 +1,61 @@
+#Create a Python file named lab_2-5.py
+#For each of the following assignment statements copy the statement 
+#as a comment, then write the other way you could write the statement 
+#using augmented assignments or not using augmented assignments. 
+#To start, set all variables used equal to 21.
+#1. x += 5
+#2. y =y - 3
+#3. z **= 2
+#4. w =w % 2
+#5. t =t / 5
+#6. a *= 12
+#7. i = i // 7
+#Be sure to include comments above each pair of 2 lines with the question 
+#number 
+#(Ex. # Question 1) and print statements beneath each assignment.\
+
+#Set up the variables equal to 21
+
+x = 21
+y = 21
+z = 21
+w = 21
+t = 21
+a = 21
+i = 21
+
+#Originial Expressions/Code
+#Question #1
+#1. x += 5
+x = x + 5
+print(x)
+
+#Question #2
+#2. y =y - 3
+y -= 3
+print(y)
+
+#Question #3
+#3. z **= 2
+z = z ** 2
+print(z)
+
+#Question #4
+#4. w =w % 2
+w %= 2
+print(w)
+
+#Question #5
+#5. t =t / 5
+t /= 5
+print(t)
+
+#Question #6
+#6. a *= 12
+a = a * 12
+print(a)
+
+#Question #7 
+#7. i = i // 7
+i //= 7
+print(i)
