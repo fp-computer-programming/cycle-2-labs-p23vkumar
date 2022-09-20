8c100c7637307cd6d2c81485540f868a0ea5edcb
Vishnu Kumar <p23vkumar@fairfieldprep.org>
2022-09-20 13:22:51 -0400

    Lab 2-6
    Lab 2-6

---
+++ lab 2-6.py
@@ -0,0 +1,36 @@
+#Create a Python file named lab_2-6.py
+#2. Set a = 5, b = 10, c = 0, d = -4
+#3. Create a conditional statement to determine if each variable is even or odd.
+#4. Print the results.
+
+#2. Set a = 5, b = 10, c = 0, d = -4
+a = 5
+b = 10
+c = 0
+d = -4
+
+#3. Create a conditional statement to determine if each variable is even or odd.
+
+#Variable a
+if a % 2 == 0:
+    print("The Number is Even")
+else:
+    print("The Number is Odd")
+
+#Variable b
+if b % 2 == 0:
+    print("The Number is Even")
+else:
+    print("The Number is Odd")
+
+#Variable c
+if c % 2 == 0:
+    print("The Number is Even")
+else:
+    print("The Number is Odd")
+
+#Variable d
+if d % 2 == 0:
+    print("The Number is Even")
+else:
+    print("The Number is Odd")
