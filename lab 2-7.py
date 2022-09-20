455f49e270cb1132ecd3fd598eef1ac6b3e26c0d
Vishnu Kumar <p23vkumar@fairfieldprep.org>
2022-09-20 13:43:36 -0400

    Lab 2-7
    Lab 2-7

---
+++ lab 2-7.py
@@ -0,0 +1,43 @@
+#ORIGINAL VARIABLES
+a = 5
+b = 10
+c = 0
+d = -4
+
+
+#1. Create a copy of lab_2-6.py and rename it lab_2-7.py
+#2. Add an additional boolean expression determining if each number 
+# is less than -5, between -5 and 5, or greater than 5.
+#3. Print the results.
+
+#Variable a
+if a < -5:
+    print("Less Than -5")
+elif a > 5:
+    print("Greater than 5")
+else:
+    print("Between Range")
+
+#Variable b
+if b < -5:
+    print("Less Than -5")
+elif b >= 5:
+    print("Greater than 5")
+else:
+    print("Between Range")
+
+#Variable c
+if c < -5:
+    print("Less Than -5")
+elif c >= 5:
+    print("Greater than 5")
+else:
+    print("Between Range")
+
+#Variable d
+if d < -5:
+    print("Less Than -5")
+elif d >= 5:
+    print("Greater than 5")
+else:
+    print("Between Range")
